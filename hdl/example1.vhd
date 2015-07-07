dfa0c085d2b2d41b3c3e78c0ec082c8f67350440
Andrei Ostanin <aostanin@gmail.com>
2015-07-08 00:13:03 +0900

    Add project files and a couple of examples

---
+++ hdl/example1.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity example1 is
+  port (
+    buttons : in  std_logic_vector(1 downto 0);
+    leds    : out std_logic_vector(7 downto 0)
+  );
+end example1;
+
+
+architecture rtl of example1 is
+begin
+
+    -- ボタンだと押す時は'0'で押さない時は'1'
+    leds <= "000000" & (not buttons);
+
+end rtl;
+
---
+++ hdl/example2.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity example2 is
+  port (
+    clk_50  : in  std_logic;
+    buttons : in  std_logic_vector(1 downto 0);
+    leds    : out std_logic_vector(7 downto 0)
+  );
+end example2;
+
+
+architecture rtl of example2 is
+
+    signal counter : std_logic_vector(31 downto 0);
+
+begin
+
+    process(clk_50)
+    begin
+        if buttons /= "11" then
+            -- ボタン押したらcounterをリセット
+            counter <= (others => '0');
+        elsif rising_edge(clk_50) then
+            -- counterを上がる
+            counter <= counter + 1;
+        end if;
+    end process;
+
+    -- counterのビット24~32をLEDで表示
+    leds <= counter(31 downto 24);
+
+end rtl;
+
